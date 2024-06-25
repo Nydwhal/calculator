e6715dd6504acd9bf911ce70af0dde716eba2a52
Nydwhal <cyril.cauquil@gmail.com>
2024-06-25 13:41:50 +0200

    refacto phase - unknown number of arguments

--- calculator.ts
+++ calculator.ts
@@ -2,8 +2,5 @@ export const calculate = (input: string): number => {
   if (input === "") {
     return 0;
   }
-  if (input.includes(",")) {
-    return input.split(",").reduce((acc, curr) => acc + Number(curr), 0);
-  }
-  return Number(input);
+  return input.split(",").reduce((acc, curr) => acc + Number(curr), 0);
 };
