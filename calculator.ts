1efc686f7c43f297a8af7216cce350e03ded46c5
Nydwhal <cyril.cauquil@gmail.com>
2024-06-25 13:51:01 +0200

    refacto phase - newline as separators

--- calculator.ts
+++ calculator.ts
@@ -2,8 +2,8 @@ export const calculate = (input: string): number => {
   if (input === "") {
     return 0;
   }
-  if (input.includes("\n")) {
-    input = input.replace("\n", ",");
-  }
-  return input.split(",").reduce((acc, curr) => acc + Number(curr), 0);
+  return input
+    .replace("\n", ",")
+    .split(",")
+    .reduce((acc, curr) => acc + Number(curr), 0);
 };
