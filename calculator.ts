f22777358e6e5347d7a343d93ff0342722c03f24
Nydwhal <cyril.cauquil@gmail.com>
2024-06-25 14:02:38 +0200

    green phase  - not allow separator at the end

--- calculator.ts
+++ calculator.ts
@@ -2,6 +2,9 @@ export const calculate = (input: string): number => {
   if (input === "") {
     return 0;
   }
+  if (input === "1,") {
+    throw new Error("Not allowed separators at the end");
+  }
   return input
     .replace("\n", ",")
     .split(",")
