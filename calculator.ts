21991f9a79d03d6a44c84cbf2183c087cbf05fbe
Nydwhal <cyril.cauquil@gmail.com>
2024-06-25 14:04:21 +0200

    refacto phase - not allow separator at the end

--- calculator.ts
+++ calculator.ts
@@ -2,7 +2,7 @@ export const calculate = (input: string): number => {
   if (input === "") {
     return 0;
   }
-  if (input === "1,") {
+  if (input[input.length - 1] === ",") {
     throw new Error("Not allowed separators at the end");
   }
   return input
