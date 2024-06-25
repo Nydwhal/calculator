ea4dbd56604362bda7355aec37fa4336a757a3bf
Nydwhal <cyril.cauquil@gmail.com>
2024-06-25 13:49:37 +0200

    green pahse - newline as separators

--- calculator.ts
+++ calculator.ts
@@ -2,5 +2,8 @@ export const calculate = (input: string): number => {
   if (input === "") {
     return 0;
   }
+  if (input.includes("\n")) {
+    input = input.replace("\n", ",");
+  }
   return input.split(",").reduce((acc, curr) => acc + Number(curr), 0);
 };
