7e7d7d0300ba251af7810cdeb1c18cae8b2530b4
Nydwhal <cyril.cauquil@gmail.com>
2024-06-25 12:29:36 +0200

    refacto phase - two strings

--- calculator.ts
+++ calculator.ts
@@ -2,8 +2,8 @@ export const calculate = (input: string): number => {
   if (input === "") {
     return 0;
   }
-  if (input.includes(",")) {
-    return 3;
+  if (input.includes(",") && input.length === 3) {
+    return Number(input[0]) + Number(input[2]);
   }
   return Number(input);
 };
