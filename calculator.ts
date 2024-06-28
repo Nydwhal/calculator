a6284b26f4689e9bc63e0c3d2d8797d5631b216e
Nydwhal <cyril.cauquil@gmail.com>
2024-06-28 12:01:24 +0200

    green phase - custom separator 4

--- calculator.ts
+++ calculator.ts
@@ -7,11 +7,16 @@ export const calculate = (input: string): number => {
   }
   if (input.startsWith("//")) {
     const separator = input.slice(2, input.indexOf("\n"));
-    input = input
-      .slice(input.indexOf("\n") + 1)
-      .split(separator)
-      .join(",");
+    input = input.slice(input.indexOf("\n") + 1);
+    // regex to check if the string contains only numbers and the separator
+    const regex = new RegExp(`[^0-9${separator}]`, "g");
+    // throw error if an item of input not match regex
+    if (regex.test(input)) {
+      throw new Error("Invalid separator");
+    }
+    input = input.split(separator).join(",");
   }
+
   return input
     .replace("\n", ",")
     .split(",")
