655d359d34446a61058d0dd54e45b5cb7ca23913
Nydwhal <cyril.cauquil@gmail.com>
2024-06-28 12:02:23 +0200

    refacto phase - custom separator 4

--- calculator.ts
+++ calculator.ts
@@ -2,18 +2,20 @@ export const calculate = (input: string): number => {
   if (input === "") {
     return 0;
   }
-  if (input[input.length - 1] === ",") {
+
+  if (input.endsWith(",")) {
     throw new Error("Not allowed separators at the end");
   }
+
   if (input.startsWith("//")) {
     const separator = input.slice(2, input.indexOf("\n"));
     input = input.slice(input.indexOf("\n") + 1);
-    // regex to check if the string contains only numbers and the separator
+
     const regex = new RegExp(`[^0-9${separator}]`, "g");
-    // throw error if an item of input not match regex
     if (regex.test(input)) {
       throw new Error("Invalid separator");
     }
+
     input = input.split(separator).join(",");
   }
 
